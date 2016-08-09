c5a11b61a2f5fe342e63c9c9d58b5fd3704726b8
AliceKass <alice.kassinger@gmail.com>
2016-08-08 18:50:35 -0600

    attempt 1 unsuccessful - carousel fix

--- JS/jquery.jcarousel.js
+++ JS/jquery.jcarousel.js
@@ -1,7 +1,6 @@
 /*! jCarousel - v0.3.4 - 2015-09-23
 * http://sorgalla.com/jcarousel/
 * Copyright (c) 2006-2015 Jan Sorgalla; Licensed MIT */
-
 (function($) {
     'use strict';
 
@@ -1468,68 +1467,3 @@
         }
     });
 }(jQuery, document));
-
-/*!
- * jQuery JavaScript Library v1.11.1
- * http://jquery.com/
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright 2005, 2014 jQuery Foundation, Inc. 
- * and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2014-05-01T17:42Z
- */
-
-$(function() {
-     $('.jcarousel')
-            .on('jcarousel:create jcarousel:reload', function() {
-            var element = $(this),
-                    width = element.innerWidth();
-
-                element.jcarousel('items').css('width', width + 'px');
-            })
-
-     .jcarousel();
-});
-
-$(function() {
-     $('.jcarousel-control-prev')
-             .on('jcarouselcontrol:active', function() {
-            $(this).removeClass('inactive');
-             })
-             .on('jcarouselcontrol:inactive', function() {
-            $(this).addClass('inactive');
-             })
-             .jcarouselControl({
-            target: '-=1'
-             });
-
-     $('.jcarousel-control-next')
-             .on('jcarouselcontrol:active', function() {
-            $(this).removeClass('inactive');
-             })
-             .on('jcarouselcontrol:inactive', function() {
-            $(this).addClass('inactive');
-             })
-             .jcarouselControl({
-            target: '+=1'
-             });
-});
-
-$(function() {
-        $('.jcarousel')
-     .jcarouselAutoscroll({
-             interval: 3000,
-             target: '+=1',
-             autostart: true
-     })
-        ;
-});
-
-$('.jcarousel').jcarousel({
-        wrap: 'circular'
-});
